238b7d76a806b2dd0aa48202ce1218c57bdf94bb
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-12-24 11:08:43 +0600

    created endpoint to get logged in user

--- task_server/index.js
+++ task_server/index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello from the server side!");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ayglwi.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -27,13 +27,66 @@ async function run() {
     const database = client.db("dev_profile");
     const usersCollection = database.collection("users");
 
+    // Create user
     app.post("/api/v1/users", async (req, res) => {
       const user = req.body;
+
+      const existigUser = await usersCollection.findOne({ email: user.email });
+
+      if (existigUser) {
+        return res.status(400).json({
+          status: "fail",
+          message: "User already exist",
+        });
+      }
+
       const resule = await usersCollection.insertOne(user);
 
       user._id = resule.insertedId;
 
       res.status(201).json({
+        status: "success",
+        data: {
+          user,
+        },
+      });
+    });
+
+    // Get a user
+
+    app.get("/api/v1/users/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: ObjectId(id) };
+
+      const user = await usersCollection.findOne(query);
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          user,
+        },
+      });
+    });
+
+    // User login
+
+    app.get("/api/v1/users", async (req, res) => {
+      const { email } = req.query;
+      const password = req.headers.password;
+
+      const filter = { email: email, password: password };
+
+      const user = await usersCollection.findOne(filter);
+
+      if (!user) {
+        return res.json({
+          status: "fail",
+          message: "Email and password dosen't match",
+        });
+      }
+
+      res.status(200).json({
         status: "success",
         data: {
           user,
