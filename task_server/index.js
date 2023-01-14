29ceb6807e2d31c1923bc7531f92487d10379153
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-14 23:46:13 +0600

    hosted refactored sever to vercel

--- task_server/index.js
+++ task_server/index.js
@@ -1,125 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-require("dotenv").config({ path: "./config.env" });
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-const app = express();
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ayglwi.mongodb.net/dev_profile?retryWrites=true&w=majority`;
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello from the server side!");
-});
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ayglwi.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-
-async function run() {
-  try {
-    const database = client.db("dev_profile");
-    const usersCollection = database.collection("users");
-
-    // Create user
-    app.post("/api/v1/users", async (req, res) => {
-      const user = req.body;
-
-      const existigUser = await usersCollection.findOne({ email: user.email });
-
-      if (existigUser) {
-        return res.status(400).json({
-          status: "fail",
-          message: "User already exist",
-        });
-      }
-
-      const resule = await usersCollection.insertOne(user);
-
-      user._id = resule.insertedId;
-
-      res.status(201).json({
-        status: "success",
-        data: {
-          user,
-        },
-      });
-    });
-
-    // Get a user
-
-    app.get("/api/v1/users/:id", async (req, res) => {
-      const id = req.params.id;
-
-      const query = { _id: ObjectId(id) };
-
-      const user = await usersCollection.findOne(query);
-
-      res.status(200).json({
-        status: "success",
-        data: {
-          user,
-        },
-      });
-    });
-
-    // User login
-
-    app.get("/api/v1/users", async (req, res) => {
-      const { email } = req.query;
-      const password = req.headers.password;
-
-      const filter = { email: email, password: password };
-
-      const user = await usersCollection.findOne(filter);
-
-      if (!user) {
-        return res.json({
-          status: "fail",
-          message: "Email and password dosen't match",
-        });
-      }
-
-      res.status(200).json({
-        status: "success",
-        data: {
-          user,
-        },
-      });
-    });
-
-    app.patch("/api/v1/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const body = req.body;
-      const updateDoc = {
-        $set: {
-          ...body,
-        },
-      };
-
-      const result = await usersCollection.updateOne(query, updateDoc);
-
-      res.status(200).json({
-        status: "success",
-        data: {
-          result,
-        },
-      });
-    });
-  } finally {
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfuly"))
+  .catch((err) => console.error(err));
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Dev profile server is listening on port ${port}`);
 });
