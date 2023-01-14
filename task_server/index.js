94c4cb06d285826c0d1f177e8287969431979989
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-14 21:25:06 +0600

    refactoring the code

--- task_server/index.js
+++ task_server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-require("dotenv").config();
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 
