3b1651bb0fe87886d4f1de418af3270fde7ee846
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-17 18:14:12 +0600

    create a fuction to catch asynchronous errors

--- task_server/controllers/userController.js
+++ task_server/controllers/userController.js
@@ -1,124 +1,91 @@
 const User = require("./../models/userModel");
 const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
 // CREATE USER OR RESIGER A USER
-exports.registerUser = async (req, res) => {
-  try {
-    const { _id } = await User.create(req.body);
+exports.registerUser = catchAsync(async (req, res, next) => {
+  const { _id } = await User.create(req.body);
 
-    const features = new APIFeatures(
-      User.findById(_id),
-      req.query
-    ).limitFields();
+  const features = new APIFeatures(User.findById(_id), req.query).limitFields();
 
-    const user = await features.query;
+  const user = await features.query;
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    console.error("💥", err);
-    res.status(500).json({
-      status: "error",
-      message: "Something very wrong.Please try again",
-    });
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
 
 // USER LOGIN TO PROFILE OR PROFILE ACCESS
-exports.loginUser = async (req, res) => {
-  try {
-    const features = new APIFeatures(
-      User.findOne(req.body),
-      req.query
-    ).limitFields();
+exports.loginUser = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(
+    User.findOne(req.body),
+    req.query
+  ).limitFields();
 
-    const user = await features.query;
+  const user = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    console.error("💥", err);
-    res.status(500).json({
-      status: "error",
-      message: "Something very wrong.Please try again",
-    });
+  if (!user) {
+    return next(new AppError("Email and password dosen't match", 401));
   }
-};
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
 
 // GET A USER
-exports.getUser = async (req, res) => {
-  try {
-    const feature = new APIFeatures(
-      User.findById(req.params.id),
-      req.query
-    ).limitFields();
+exports.getUser = catchAsync(async (req, res, next) => {
+  const feature = new APIFeatures(
+    User.findById(req.params.id),
+    req.query
+  ).limitFields();
 
-    const user = await feature.query;
+  const user = await feature.query;
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    console.error("💥", err);
-    res.status(500).json({
-      status: "error",
-      message: "Something very wrong.Please try again",
-    });
+  if (!user) {
+    return next(new AppError("Can't find the user", 400));
   }
-};
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
 
 // UPDATE A USER
-exports.updateUser = async (req, res) => {
-  try {
-    const options = { runValidators: true, new: true };
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const options = { runValidators: true, new: true };
 
-    const features = new APIFeatures(
-      User.findByIdAndUpdate(req.params.id, req.body, options),
-      req.params
-    ).limitFields();
+  const features = new APIFeatures(
+    User.findByIdAndUpdate(req.params.id, req.body, options),
+    req.params
+  ).limitFields();
 
-    const user = await features.query;
+  const user = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    console.error("💥", err);
-    res.status(500).json({
-      status: "error",
-      message: "Something very wrong.Please try again",
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
 
 // DELETE A USER
-exports.deleteUser = async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    console.error("💥", err);
-    res.status(500).json({
-      status: "error",
-      message: "Something very wrong.Please try again",
-    });
-  }
-};
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
