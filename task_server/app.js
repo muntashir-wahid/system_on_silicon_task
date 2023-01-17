f9f5c4e93d84e4bb31473ac14dfefe6a6d86d675
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-17 14:48:50 +0600

    created a global error handling middleware to handle all application errors in one place

--- task_server/app.js
+++ task_server/app.js
@@ -12,10 +12,22 @@ app.use(morgan("dev"));
 
 app.use("/api/v1/users", userRouter);
 
-app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on the server`,
+app.all("*", (req, _, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on the server`);
+
+  err.statusCode = 404;
+  err.status = "fail";
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
